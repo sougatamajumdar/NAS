f3913a65002609446f88bb860a9a9dde745396c0
sougatamajumdar <159818664+sougatamajumdar@users.noreply.github.com>
2026-06-01 09:49:06 +0530

    Update settings.py

--- nas_backend/nas_backend/settings.py
+++ nas_backend/nas_backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9un=wgdxm#&r@)$pb+6k45t+3uyjs0(-_0y09m63ocdqsmu2x*'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
